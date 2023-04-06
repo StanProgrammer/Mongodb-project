e4eb845a928f27d31ec2ece70ba5999af8c0d850
AtibProgrammer <atibkhan392@gmail.com>
2023-04-06 13:44:17 +0530

    Mongodb database connection

--- util/database.js
+++ util/database.js
@@ -1,8 +1,21 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
-
-module.exports = sequelize;
+const mongodb = require('mongodb');
+const MongoClient=mongodb.MongoClient
+const mongoConnect=(cb)=>{
+  MongoClient.connect('mongodb+srv://atikhan:0rsYoszLRAZiJ7O1@cluster0.e91g1kn.mongodb.net/?retryWrites=true&w=majority')
+    .then((client) => {
+      console.log('Connected');
+      cb(client)
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+}
+module.exports=mongoConnect
+// const MongoClient=mongodb.MongoClient;
+// MongoClient.connect('mongodb+srv://atikhan:0rsYoszLRAZiJ7O1@cluster0.e91g1kn.mongodb.net/?retryWrites=true&w=majority')
+// .then(()=>{
+//   console.log('Connected');
+// })
+// .catch((error)=>{
+//   console.log(error);
+// })

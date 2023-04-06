803ae3130b74d9c5198befdde9e25f97d94a2de0
AtibProgrammer <atibkhan392@gmail.com>
2023-04-06 16:21:57 +0530

    Bug Fixed

--- models/product.js
+++ models/product.js
@@ -8,7 +8,7 @@ class Product{
     this.price=price,
     this.description=description,
     this.imageUrl=imageUrl,
-      this._id = new mongodb.ObjectId(id)
+      this._id = id ? new mongodb.ObjectId(id): null;
   }
   save(){
     const db = getDb()
@@ -48,6 +48,16 @@ class Product{
       .then(product=>{
         console.log(product);
         return product
+      })
+      .catch(err=>{
+        console.log(err);
+      })
+    }
+    static deleteById(prodId){
+      const db=getDb()
+      return db.collection('products').deleteOne({_id:new mongodb.ObjectId(prodId)})
+      .then(res=>{
+        console.log('Deleted');
       })
       .catch(err=>{
         console.log(err);

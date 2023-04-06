120b2941785c930285cc115c10e6dacf3d5c158d
AtibProgrammer <atibkhan392@gmail.com>
2023-04-06 15:26:38 +0530

    findByid Achieved

--- models/product.js
+++ models/product.js
@@ -1,5 +1,6 @@
 const mongoConnect=require('mongodb')
 const getDb = require('../util/database').getDb;
+const mongodb=require('mongodb')
 
 class Product{
   constructor(title,price,description,imageUrl) {
@@ -24,6 +25,19 @@ class Product{
       .then(products=>{
         console.log(products);
         return products
+      })
+      .catch(err=>{
+        console.log(err);
+      })
+    }
+    static findById(prodId){
+      const db=getDb()
+      return db.collection('products')
+      .find({_id:new mongodb.ObjectId(prodId)})
+      .next()
+      .then(product=>{
+        console.log(product);
+        return product
       })
       .catch(err=>{
         console.log(err);

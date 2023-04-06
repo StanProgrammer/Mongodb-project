e632eef1d274f57730ebad331bfb5ff08d59fad9
AtibProgrammer <atibkhan392@gmail.com>
2023-04-06 15:02:53 +0530

    FetchAll Implemented

--- models/product.js
+++ models/product.js
@@ -15,9 +15,22 @@ class Product{
     .catch((error)=>{
       console.log(error);
     })
-
   }
-}
+    static fetchAll(){
+      const db=getDb();
+      return db.collection('products')
+      .find()
+      .toArray()
+      .then(products=>{
+        console.log(products);
+        return products
+      })
+      .catch(err=>{
+        console.log(err);
+      })
+    }
+  }
+
 
 
 module.exports = Product;
